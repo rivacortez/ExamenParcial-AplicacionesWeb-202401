a07e52071863bb1232ea3999529747e7611a6bc0
rivacortezf <u202215313@upc.edu.pe>
2024-05-08 18:15:32 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import ToolbarComponent from "./public/components/Toolbar.component.vue";
+
+export default {
+  components: {
+    ToolbarComponent
+  }
+}
+</script>
+
+<template>
+  <ToolbarComponent/>
+
+  <RouterView />
+</template>
---
+++ src/public/components/Toolbar.component.vue
@@ -0,0 +1,24 @@
+<script>
+import Toolbar from 'primevue/toolbar'
+import Button from 'primevue/button'
+
+export default {
+  components: {
+    Toolbar,
+    Button
+  },
+  name: 'Toolbar',
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+</template>
+
+<style scoped>
+
+</style>
+
+
